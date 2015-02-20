f5955e5aaf290d08f28f14810b607a07cfb2c42c
kevinwalstrom <noreply>
2015-02-20 21:49:40 +0000

    Add component pikaday

---
+++ ui/pikaday.reel/pikaday.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/pikaday.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Pikaday
+ * @extends Component
+ */
+exports.Pikaday = Component.specialize(/** @lends Pikaday# */ {
+    constructor: {
+        value: function Pikaday() {
+            this.super();
+        }
+    }
+});
